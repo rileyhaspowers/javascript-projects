f60379c2e6b19ac515588c3fe2412980dd5f68fa
rileyhaspowers <65466050+rileyhaspowers@users.noreply.github.com>
2024-01-08 16:00:39 -0600

    ASSIGNMENT-data-and-variables-exercises.js

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,31 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let shuttleName = "Determination";
+let shuttleSpeed = 17500;
+let marsKiloDistance = 225000000;
+let moonDistance = 38400;
+const milesPerKilo = 0.621;
+
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeed)
+console.log(typeof marsKiloDistance);
+console.log(typeof moonDistance);
+console.log(typeof milesPerKilo);
+
+let milesToMars;
+milesToMars = (marsKiloDistance * milesPerKilo);
+let hoursToMars = (milesToMars / shuttleSpeed);
+let daysToMars;
+daysToMars = (hoursToMars / 24);
+
+console.log(shuttleName  + " will take " + daysToMars + " days to reach Mars.");
+
+let milesToMoon;
+milesToMoon = (moonDistance * milesPerKilo);
+let hoursToMoon = (milesToMoon / shuttleSpeed);
+let daysToMoon;
+daysToMoon = (hoursToMoon / 24);
+
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.");
